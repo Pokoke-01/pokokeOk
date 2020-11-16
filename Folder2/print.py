f78aacb341fce53f32bb0ca37eff9f9b10ba7742
Pokoke <74485230+Pokoke-01@users.noreply.github.com>
2020-11-17 06:00:31 +0700

    Add files via upload

---
+++ Folder2/print.py
@@ -0,0 +1,11 @@
+#!/bin/user/python2
+
+
+print ("=====================================")
+print ("H e l l o !")
+print ("selamat datang di pemrograman python2")
+print ("belajar pemrograman dasar python")
+print ("menggunakan nano editor, Termux")
+print ("-------------------------------------")
+print ("s e m a n g a t . . . !")
+print ("=====================================")
