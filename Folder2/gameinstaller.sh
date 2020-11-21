248cd0b906429f8b56a71d4bfb86b75d98814896
Pokoke <74485230+Pokoke-01@users.noreply.github.com>
2020-11-21 07:06:38 +0700

    Add files via upload

---
+++ Folder2/gameinstaller.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#-----------GameInstaller------------
+#
+
+
+
+
+clear
+figlet DonlotGame
+echo "================================================="
+echo
+echo
+echo
+echo
+echo
+echo "===================================================="
+
+echo "menu:"
+echo "1.Install nSnake"
+echo "2.Install Moon Buggy"
+
+read -p 'pilih: ' pil;
+
+if [ $pil = '1' ]
+then
+    cd $home
+    pkg update -y
+    pkg upgrade
+    pkg install nsnake
+
+else
+    echo "Wrong input, Masukkan pilihan yang benar"
+
+
+fi
+
